36e69a89793c5cd92f1caf11b59fbfe31ae0c97a
deadlock314 <chandansoni2270@gmail.com>
2022-12-01 21:50:54 +0530

    modified nav and footer structure

--- src/Components/NavComponents/NavBar.jsx
+++ src/Components/NavComponents/NavBar.jsx
@@ -3,7 +3,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars, faXmark, faSearch } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react';
 import './NavBar.css';
-
+import {Link} from 'react-router-dom';
 
 library.add(faBars, faXmark, faSearch);
 
@@ -28,12 +28,18 @@ export default function NavBar() {
           <img className="navicon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9ceK4DRKMgME3VEXa-oi9IAQl8WiJkFsac3iIx0edOpU8T5RZKYKwTxSdpZrSo7LT_lA&usqp=CAU" />
           <p className="company-name">P2PEnergyTradingPlatform </p>
         </div>
-
-
-        <div className="profile-div">
-          <img id="profile-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb_kv5K0jzuBD-u0Zoun7t7jxWJv4Wdmc6H2YuLnXC7L3WmVS7QToqrf8ivA8SMYmHBxg&usqp=CAU" />
-          <p>Harshita Shukla</p>
+        <div className="nav-div1">
+          <Link to="/signup" >Sign Up</Link>
         </div>
+        <div className="nav-div2">
+          <Link to="/login" >LogIn</Link>
+        </div>
+
+
+        {/* <div className="nav-div">
+          <img id="nav-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb_kv5K0jzuBD-u0Zoun7t7jxWJv4Wdmc6H2YuLnXC7L3WmVS7QToqrf8ivA8SMYmHBxg&usqp=CAU" />
+          <p>Harshita Shukla</p>
+        </div> */}
 
       </nav>
     </>
